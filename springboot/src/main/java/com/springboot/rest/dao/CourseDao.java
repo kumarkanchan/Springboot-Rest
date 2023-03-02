a8eba6f52d3eebfdaf44bab9b28ee05adebd9532
Kanchan <info.kanchankumar@gmail.com>
2023-03-02 15:08:56 +0530

    Commit-2

--- springboot/src/main/java/com/springboot/rest/dao/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.rest.dao;
-
-import com.springboot.rest.entity.Courses;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CourseDao extends JpaRepository<Courses, Long> {
-
-
-}
